4655f680cb982715f28aa3b071d22887b8829433
kaoxiong20 <kaoxiong520@gmail.com>
2023-04-15 11:06:19 -0500

    Created the following components: About, Body, Today, PastEntries, EmojiPicker to create functionality and route the pages when clicking on the Nav Bar

---
+++ src/components/EmojiPicker.js
@@ -0,0 +1,9 @@
+import EmojiPicker from 'emoji-picker-react';
+
+function App() {
+  return (
+    <div>
+      <EmojiPicker height={500} width={900} />
+    </div>
+  );
+}
--- src/components/Today.js
+++ src/components/Today.js
@@ -4,9 +4,11 @@ import styled from 'styled-components';
 
 const Today = () => (
   <div className="main-content">
+    <br/>
     <StyledH2>Today's Mood Entries</StyledH2>
-    <p>Enter your entries for today's mood.</p>
-
+    <StyledP>Enter your entries for today's mood.</StyledP>
+    <StyledP>Think about: Who? What? Where? Why?</StyledP>
+    <StyledP>Where does this stem from?</StyledP>
     <NewInput />
   </div>
 );
@@ -14,5 +16,19 @@ const Today = () => (
 export default Today;
 
 const StyledH2 = styled.h2`
+    font-size: 20px;
+    background-color: #A2ABCE;
+    padding: 20px 30px 20px 30px;
+    text-align: center;
+    margin: auto;
+    width: 400px;
+    text-transform: uppercase;
+`
 
+const StyledP = styled.p`
+    font-size: 15px;
+    padding: 20px 30px 20px 30px;
+    text-align: center;
+    margin: auto;
+    width: 400px;
 `
